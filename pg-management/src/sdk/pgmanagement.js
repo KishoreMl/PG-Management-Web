001d8f2234ec3eeb9534c293ae5e2df1ab6e78a6
KISHORE M L <kishoreml2909@gmail.com>
2023-12-18 20:52:24 +0530

    API Wrapper setup

---
+++ pg-management/src/sdk/pgmanagement.js
@@ -0,0 +1,49 @@
+
+
+export async function createPgBranch(pg) {
+    
+}
+
+export async function getPgBranch(pgId) {
+
+}
+
+export async function updatePgBranch(pgId) {
+    
+}
+
+export async function deletepgBranch(pgId) {
+    
+}
+
+export async function createGuest(guest) {
+    
+}
+
+export async function getGuest(guestId) {
+    
+}
+
+export async function updateGuest(guestId) {
+    
+}
+
+export async function deleteGuest(guestId) {
+    
+}
+
+export async function createRoom(room) {
+    
+}
+
+export async function getRoom(roomId) {
+    
+}
+
+export async function updateRoom(roomId) {
+    
+}
+
+export async function deleteRoom(roomId) {
+    
+}
