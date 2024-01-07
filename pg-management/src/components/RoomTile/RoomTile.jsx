769a7f81eda87900c666098c57636a32fc076eac
KISHORE M L <kishoreml2909@gmail.com>
2024-01-07 13:27:56 +0530

    dropdown list toggle logic

--- pg-management/src/components/RoomTile/RoomTile.jsx
+++ pg-management/src/components/RoomTile/RoomTile.jsx
@@ -7,31 +7,46 @@ export class RoomTile extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            dropdown: "",
+            showDropdown: "",
             selected:false,
         }
+         this.dropdownRef = React.createRef();
     }
+
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside);
+    }
+
+    handleClickOutside = (event) => {
+        if (this.dropdownRef && !this.dropdownRef.current.contains(event.target)) {
+            this.setState({ showDropdown: false });
+        }
+    }
+
+    handleShowDropdown() {
+        this.state.showDropdown?this.setState({ showDropdown:false }) :this.setState({ showDropdown: true })
+    }
+
+    handleRoomSelect() {
+        this.setState({ selected: this.state.selected ? false : true });
+        this.props.onRoomSelect(this.props.room.roomId)
+    }
+
     render() {
         return (
             <div
                 className={`room-tile ${this.state.selected ? 'selected' : ''}`}
-                onClick={() => {
-                    this.setState({ selected: this.state.selected ? false : true });
-                    this.props.onRoomSelect(this.props.room.roomId)
-                }}>
+                onClick={() => this.handleRoomSelect()}>
                 <div className="tile-header">
                     <p>{this.props.room.number}</p>
                     {this.props.room.type === 'AC' && <p className="badge">{this.props.room.type}</p>}
-                    <div className="tile-header-left">
+                    <div className="tile-header-left" ref={this.dropdownRef}>
                         <IconMap
                             icon="more"
-                            onClick={() => {
-                                this.state.dropdown === "" ?
-                                this.setState({ dropdown: "show" }) :
-                                this.setState({ dropdown: "" })
-                            }}
+                            onClick={() => this.handleShowDropdown()}
+                            
                         /> 
-                        <div className={`dropdown-list ${this.state.dropdown}`}>
+                        <div className={`dropdown-list ${this.state.showDropdown?'show':''}`}>
                             <div className="list-option" onClick={() => this.props.onTileOptionSelect('Add Guest')}>Add Guest</div>
                             <div className="list-option" onClick={() => this.props.onTileOptionSelect('Remove Guest')}>Remove Guest</div>
                             <div className="list-option" onClick={() => this.props.onTileOptionSelect('Edit Room')}>Edit Room</div>
