138b92f293aed100022e3293eaab8c581849ad02
KISHORE M L <kishoreml2909@gmail.com>
2024-03-16 12:15:27 +0530

    Drag and Drop test

--- pg-management/src/components/GuestCard/GuestCard.jsx
+++ pg-management/src/components/GuestCard/GuestCard.jsx
@@ -3,14 +3,14 @@ import './GuestCard.scss';
 import user from '../../static/guest.png';
 
 export class GuestCard extends React.Component{
-    constructor(props) {
-        super(props);    
-    }
+    
     render(){
         return (
-            <div className={`guest-card ${this.props.guest?this.props.guest.rentPaid?'paid':'not-paid':''}`}>
+            <div
+                className={`guest-card ${this.props.guest ? this.props.guest.rentPaid ? 'paid' : 'not-paid' : ''}`}
+                draggable={true}>
                 <div className='guest-img'>
-                    <img src={user}></img>
+                    <img src={user} alt=''></img>
                 </div>
                 <div className='guest-details'>
                     <p>{this.props.guest?this.props.guest.name:'Available'}</p>
